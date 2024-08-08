019306b9d8fd0227999dd4ff46401209ae859970
Lamiaa Selim <lamiaaselim1896@gmail.com>
2024-08-08 20:38:43 +0300

    upload day04

---
+++ Day04/Model/departmentModel.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose");
+
+
+const schema =new mongoose.Schema({
+    _id:Number,
+    name:String
+})
+
+
+
+//mapping
+module.exports=mongoose.model("departments",schema);
